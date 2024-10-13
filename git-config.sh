a3a1c6653fe002266af4dba3b9d0bdbe5bf023c4
LukasJurena <jurenalukash@gmail.com>
2024-10-13 09:53:19 +0200

    Hello World

---
+++ git-config.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git config --list
+git config --global user.name "LukasJurena" 
+git config --global user.email "jurenalukash@gmail.com"
+echo "Přihlášení uživatele proběhlo v pořádku"
+#cd eshop_app/
+#"https://fonts.bunny.net"
+#fontshare
+#sh git_config.sh
+#composer create-project laravel/laravel hokuspokus
+#ls
+#di
